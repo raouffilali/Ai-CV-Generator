f6ab83f54789390c23f9137f4b33b49d9d345ad7
Raouf Filali <filaliabderaouf@yahoo.com>
2024-06-29 01:49:56 +0100

    chore: Update resume creation page with improved text and formatting

--- frontend/src/dashboard/Dashboard.jsx
+++ frontend/src/dashboard/Dashboard.jsx
@@ -30,7 +30,7 @@ function Dashboard() {
   return (
     <div className="p-10 md:px-20 lg:px-32">
       <h2 className="font-bold text-3xl">My Resume</h2>
-      <p>Start Creating AI CV for your next Job role</p>
+      <p>Start Creating AI CV for your next Job role ✨</p>
       <div
         className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5
       mt-10
